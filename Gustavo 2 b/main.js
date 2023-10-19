e4a6dc863e4a9d95129592aa3f7a370a82f144bd
GustavoMoraisGoncalves <135244807+GustavoMoraisGoncalves@users.noreply.github.com>
2023-10-19 12:00:19 -0300

    Add files via upload

--- Gustavo 2 b/main.js
+++ Gustavo 2 b/main.js
@@ -1,5 +1,67 @@
 function TocaSomPom () {
-   Document.querySelector('#som_tecla_pom').play();
+     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = TocaSomPom();
+document.querySelector('.tecla_pom').onclik = TocaSomPom;
+
+ 
+function TocaSomClap() {
+   document.querySelector('#som_tecla_Clap').play();
+}
+
+document.querySelector('.tecla_clap').onclik = TocaSomClap;
+
+
+
+function TocaSomTim () {
+   document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclik = TocaSomTim;
+
+
+function TocaSomPuff () {
+   document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclik = TocaSomPuff;
+
+
+
+function TocaSomSplash () {
+   document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_Splash').onclik = TocaSomSplash;
+
+
+
+function TocaSomToim () {
+   document.querySelector('#som_tecla_Toim').play();
+}
+
+document.querySelector('.tecla_Toim').onclik = TocaSomToim;
+
+
+
+function TocaSomPsh () {
+   document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclik = TocaSomPsh;
+
+
+
+function TocaSomTic () {
+   document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclik = TocaSomTic;
+
+
+
+function TocaSomTsom () {
+   document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclik = TocaSomTom;
